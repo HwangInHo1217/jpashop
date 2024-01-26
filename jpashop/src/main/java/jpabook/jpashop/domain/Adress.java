1b6c5b01d4ffedcfe7b0899d43007e6390a24cb7
HwangInHo1217 <hih1217@skuniv.ac.kr>
2024-01-26 23:04:05 +0900

    domain 완성

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Adress.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Adress {
+
+    private  String city;
+    private String street;
+    private String zipcode;
+    protected Adress(){
+
+    }
+    public Adress(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
